9f1560c3321fa239ec864dfbb0da4bc1940ecdac
queen-cece <ccezeonu3@gmail.com>
2018-07-27 18:09:40 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,7 +1,8 @@
 # Code your cash register here
 
 class AmazonTransaction
-  def initialize(total=0, items =[], discount) 
+  attr_accessor :total, :items, :discount
+  def initialize(total=0, items =[], discount=0) 
     @total = total
     @items = items
     @discount = discount
